55cfb20c71632fb3e6661a577a352f678ecedd0e
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-10-13 15:47:32 -0400

    💣 CODIFICAÇÃO Finalizando estilização da exibição do Viewer

--- src/components/Dashboard Initial/Viewer/ViewerPerson.js
+++ src/components/Dashboard Initial/Viewer/ViewerPerson.js
@@ -1,7 +1,15 @@
-export default function ViewerPerson(){
+import './ViewerPerson.css';
+
+
+export default function ViewerPerson({name, email}){
 
     return(
-        <>
-        </>
+        <div className="divBackground">
+            <span className='spanImagePerson'/>
+            <div>
+                <p>{name}</p>  
+                <p>{email}</p>  
+            </div>
+        </div>
     )
 }
