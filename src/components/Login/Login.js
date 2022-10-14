afd7756e53b2de531e1cb768fa741e0fb1a98417
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-10-14 08:25:00 -0400

    💣 CODIFICAÇÃO Ajustando o código para deixar mais visível Ainda não foi inserado armanezamento no LocalStorage e verificação de usuário

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,30 +6,33 @@ export default function Login(){
 
     return(
         <>
-            <section className='sectionExbForm'>
-                <h1>Welcome Back</h1>
-                <h2>Continue with Google or enter your details to <br/> get sign in to your account</h2>
-                <button>
-                <img src="https://img.icons8.com/color/24/000000/google-logo.png"/>
-                    <p>Log in with Google</p>
-                </button>
-                <span>
-                    <div>
-                        <p>or</p>    
-                    </div>
-                </span>
+            <section className='sectionGeneral'>
+                <aside className='asideExbForm'>
+                    <h1>Welcome Back</h1>
+                    <h2>Continue with Google or enter your details to <br/> get sign in to your account</h2>
+                    <button>
+                    <img src="https://img.icons8.com/color/24/000000/google-logo.png"/>
+                        <p>Log in with Google</p>
+                    </button>
+                    <span>
+                        <div>
+                            <p>or</p>    
+                        </div>
+                    </span>
 
-                <form>
-                    <input type='email' placeholder='Email'/>
-                    <input type='password' placeholder='Password'/>
-                    <button>Forgot password</button>
-                    <input type='submit' id='btn-login' value='Log in' placeholder='Log in'/>
-                </form>
-            </section>
-            <section className='sectionExbDecoration'>
-                <i/>
-                <p>Copyrigth @room 2022 | <a>Privacy Policy</a></p>
-            </section>
+                    <form>
+                        <input type='email' placeholder='Email'/>
+                        <input type='password' placeholder='Password'/>
+                        <button>Forgot password</button>
+                        <input type='submit' id='btn-login' value='Log in' placeholder='Log in'/>
+                    </form>
+                </aside>
+
+                <aside className='asideExbDecoration'>
+                    <i/>
+                    <p>Copyrigth @room 2022 | <a>Privacy Policy</a></p>
+                </aside>    
+            </section>  
         </>
     )
 }
