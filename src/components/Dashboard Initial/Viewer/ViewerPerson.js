d4275488a7854529b2038365ba3f93aa8b20b2ef
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-10-14 08:23:54 -0400

    💣 CODIFICAÇÃO Editando a estilização da exibição dos usuários no dashboard

--- src/components/Dashboard Initial/Viewer/ViewerPerson.js
+++ src/components/Dashboard Initial/Viewer/ViewerPerson.js
@@ -1,15 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { deleteUser, editUser } from '../../../features/Users';
+import EditUser from '../Edit User/EditUser';
+
 import './ViewerPerson.css';
 
 
-export default function ViewerPerson({name, email}){
+export default function ViewerPerson({id, name, email, user, image}){
+
+    const dispatch = useDispatch();
+
+    function editUser({name, email, user}){
+        const editUser = <EditUser name={name} email={email} user={user}/>
+
+        console.log(editUser.props);
+    }
+
+    console.log( user, name, image);
 
     return(
-        <div className="divBackground">
-            <span className='spanImagePerson'/>
+        <div className="divBackground" id='divBackground'>
+            <h1>{id}</h1>
+            <span style={{backgroundImage: `url("${image}")`}} className='spanImagePerson'/>
             <div>
                 <p>{name}</p>  
-                <p>{email}</p>  
+                <p>Email: {email}</p>  
             </div>
+            <button id='btnDeleteUser' onClick={() => {dispatch(deleteUser({id: id}))}}>Deletar Usuário</button>
+            <button id='btnEditUser' onClick={editUser({email: 'teste'})}>Visualizar e Editar</button>
         </div>
     )
 }
