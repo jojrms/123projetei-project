3cf170a0a1aedc632e51281430c292437fbcc7dc
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-10-14 08:36:37 -0400

    Alterando as imagens que serão exibidas no perfil das pessoas fakes cadastradas

--- src/FakeData.js
+++ src/FakeData.js
@@ -11,13 +11,13 @@ export const UsersData = [
         name: "Andina Souza",
         username: "And_sza",
         email: "andsza@gmail.com",
-        urlPhoto: "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=562&q=80",
+        urlPhoto: "https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80",
     },
     {
         id: 3,
         name: "Cleo Gomez",
         username: "Clecleo",
         email: "cleogp@gmail.com",
-        urlPhoto: "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=562&q=80",
+        urlPhoto: "https://images.unsplash.com/photo-1531123897727-8f129e1688ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
     }
 ]
