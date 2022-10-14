377c3b415189d215ad35e76c98f0af96f07d1b46
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-10-14 11:01:08 -0400

    💣 CODIFICAÇÃO Chamando código de login no App.js Adicionando uma rota para levar o usuário ao login sempre que tentar colocar uma url diferente

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import './components/General.css'
 
 import Login from './components/Login/Login';
 import DashboardInitial from './components/Dashboard Initial/DashboardInitial';
+import { AuthProvider } from './context/auth';
+import useAuth from './hooks/useAuth';
 
+const Private = ({ Item }) => {
+  const signed = false;
+  return signed > 0 ? <DashboardInitial/> : <Login/>
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-            <Route path="/" element={<Login/>}/>
-            <Route path="/DashboardInitial" element={<DashboardInitial/>}/>
-        </Routes>
-      </div> 
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+              <Route path="/" element={<Login/>}/>
+              <Route path="*" element={<Login/>}/>
+              <Route path="/DashboardInitial" element={<DashboardInitial/>}/>
+              <Route path="/DashboardInitial/:id" element={<DashboardInitial/>}/>
+          </Routes>
+        </div> 
+      </BrowserRouter>  
+    </AuthProvider>
+    
     
   );
 }
