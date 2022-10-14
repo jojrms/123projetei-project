e8533b4dd6e956c5ade33096c55ae04359730ee5
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-10-14 08:20:40 -0400

    💣 CODIFICAÇÃO Criando dados fakes que serão exibidos na listagem dos usuários As informações inseridas não são as que permanecerão

---
+++ src/FakeData.js
@@ -0,0 +1,23 @@
+export const UsersData = [
+    {
+        id: 1,
+        name: "Lee S. Yung",
+        username: "Lee",
+        email: "leey@gmail.com",
+        urlPhoto: "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=562&q=80",
+    },
+    {
+        id: 2,
+        name: "Andina Souza",
+        username: "And_sza",
+        email: "andsza@gmail.com",
+        urlPhoto: "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=562&q=80",
+    },
+    {
+        id: 3,
+        name: "Cleo Gomez",
+        username: "Clecleo",
+        email: "cleogp@gmail.com",
+        urlPhoto: "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=562&q=80",
+    }
+]
