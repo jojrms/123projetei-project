1e5de741d1bd763447f8b5a9fdcc42b2ea92eac8
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-10-16 09:41:41 -0400

    💣 CODIFICAÇÃO Corrigindo função que exclui usuários

--- src/components/Dashboard Initial/Viewer/ViewerPerson.js
+++ src/components/Dashboard Initial/Viewer/ViewerPerson.js
@@ -1,15 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { useState } from 'react';
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
-import { deleteUser, editUser } from '../../../features/Users';
-import EditUser from '../Edit User/EditUser';
+import { deleteUser } from '../../../features/Users';
 
 import './ViewerPerson.css';
 
 
 export default function ViewerPerson({id, name, email, user, image}){
 
-    const dispatch = useDispatch();
     const navigate = useNavigate();
 
     // Função que, ao clicar no botão de edição, direciona o usuário
@@ -17,6 +14,32 @@ export default function ViewerPerson({id, name, email, user, image}){
     function editUser(){
         navigate(`/DashboardInitial/edit-user/${id}`)
     }
+
+    // Função para excluir usuário da lista
+    const deleteUser = (id) => {
+
+        const identification = id;
+        console.log(identification, 'identificação do usuário');
+
+        // Puxa a chave "newUser" para fazer verificação de sua existência
+        const newUser = JSON.parse(localStorage.getItem('newUser'));
+
+        // Puxa a chave "users" para fazer verificação de sua existência
+        const users = JSON.parse(localStorage.getItem('newUser'));
+
+        // Se existir a chave "newUsers" no localStorage, executa
+        if(newUser) {
+
+            // Verifica o index do objeto que será deletado da array
+            const index = newUser.findIndex( (element) => element.id === identification );
+            // Exclui o usuário da array
+            newUser.splice(index, 1);
+            // Substitui a array do LocalStorage pela nova, com o 
+            // usuário excluido
+            localStorage.setItem('newUser', JSON.stringify(newUser))
+        }
+    }
+
 
 
     return(
@@ -29,7 +52,7 @@ export default function ViewerPerson({id, name, email, user, image}){
             </div>
 
             {/* BOTÃO COM FUNÇÃO DE DELETAR O USUÁRIO */}
-            <button id='btnDeleteUser' onClick={() => {dispatch(deleteUser({id: id}))}}>Deletar Usuário</button>
+            <button id='btnDeleteUser' onClick={() => {deleteUser(id)}}>Deletar Usuário</button>
             <button id='btnEditUser' onClick={editUser}>Visualizar e Editar</button>
         </div>
     )
